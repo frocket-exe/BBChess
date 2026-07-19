da8616cf650581348a1a5a47295db83a87589c71
frocket <freddie.rockett@gmail.com>
2026-07-20 00:08:09 +0100

    Added pages

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const port = 3000;
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('../views/index');
 });
 
 app.listen(port, () => {

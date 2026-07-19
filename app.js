f7630c52d77ab9ee04cfca5271733a733c4f8ccc
frocket <freddie.rockett@gmail.com>
2026-07-20 00:32:38 +0100

    Added 3 different pages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+const path = require('path');
 const port = 3000;
 
 // app.get('/', (req, res) => {
@@ -8,7 +8,15 @@ const port = 3000;
 // });
 
 app.get('/', (req, res) => {
-  res.render('../views/index');
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/players', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/players.html'));
+});
+
+app.get('/tournaments', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/tournaments.html'));
 });
 
 app.listen(port, () => {

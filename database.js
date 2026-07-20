f17e41ce8bc65b8eae4d1125853378e56ef9edff
frocket-exe <freddie.rockett@gmail.com>
2026-07-20 15:55:11 +0100

    Added all game data and a getStats function

--- database.js
+++ database.js
@@ -16,5 +16,23 @@ async function getPlayers() {
     return players;
 }
 
+async function getStats(playerID) {
+    const db = await openDB();
+    const gameResults = await db.all(`SELECT whiteID, blackID, result FROM games WHERE whiteID = ${playerID} OR blackID = ${playerID}`);
+    let draws = 0;
+    let wins = 0;
+    let losses = 0;
+    gameResults.forEach(game => {
+        if(game.result == '1/2-1/2') {
+            draws ++;
+        } else if ((game.result == '1-0' && game.whiteID == playerID) || (game.result == '0-1' && game.blackID == playerID)) {
+            wins ++;
+        } else {
+            losses ++;
+        }
+    });
+    return [wins, draws, losses];
+}
 
-module.exports = getPlayers;
+
+module.exports = {getPlayers, getStats};

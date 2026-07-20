d0505e26c35708f8325a2cbb99694836ff7363d2
frocket-exe <freddie.rockett@gmail.com>
2026-07-20 12:11:21 +0100

    Made player cards from SQL

--- app.js
+++ app.js
@@ -2,22 +2,37 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const asyncHandler = require('express-async-handler');
+
+const getPlayers = require('./database');
 
 app.use(express.static('public'));
 app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.get('/players', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/players.html'));
-});
+app.get('/players', asyncHandler(async (req, res) => {
+    const playerData = await getPlayers();
+    if(!playerData) {
+        res.status(404);
+        throw new Error('Players not found');
+    } else {
+        res.render('players', {playerData: playerData});
+    }
+}));
+
+// app.get('/players', (req, res) => {
+//     const playerData = getPlayers();
+//     console.log(playerData);
+//     res.render('players', {playerData: playerData});
+// });
 
 app.get('/tournaments', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/tournaments.html'));
+    res.sendFile(path.join(__dirname, '/views/tournaments.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
